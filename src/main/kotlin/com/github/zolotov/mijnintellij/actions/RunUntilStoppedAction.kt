362b01a562a1ebd49ed9704e5e47b6510929656f
Alexander Zolotov <zolotov@jetbrains.com>
2021-07-20 18:16:01 +0200

    More untilStopped/once actions

--- src/main/kotlin/com/github/zolotov/mijnintellij/actions/RunUntilStoppedAction.kt
+++ src/main/kotlin/com/github/zolotov/mijnintellij/actions/RunUntilStoppedAction.kt
@@ -1,31 +1,27 @@
-package com.github.zolotov.mijnintellij.listeners
+package com.github.zolotov.mijnintellij.actions
 
-import com.intellij.execution.JavaRunConfigurationBase
+import com.intellij.execution.Executor
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.actions.ConfigurationContext
 import com.intellij.execution.actions.RunContextAction
 import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.executors.DefaultDebugExecutor
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.junit.JUnitConfiguration
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.Presentation
-import com.intellij.openapi.components.service
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManagerListener
 import com.intellij.rt.execution.junit.RepeatCount
 
-class RunUntilStoppedAction() : RunContextAction(DefaultRunExecutor.getRunExecutorInstance()) {
+abstract class RunDebugUntilStoppedAction(private val repeatMode: String, executor: Executor) : RunContextAction(executor) {
     override fun perform(configuration: RunnerAndConfigurationSettings, context: ConfigurationContext) {
         val junitConfiguration = configuration.configuration as? JUnitConfiguration
         if (junitConfiguration != null) {
-            junitConfiguration.repeatMode = RepeatCount.UNLIMITED
+            junitConfiguration.repeatMode = repeatMode
         }
         super.perform(configuration, context)
     }
 
     override fun isEnabledFor(configuration: RunConfiguration): Boolean {
-        return super.isEnabledFor(configuration) && configuration is JUnitConfiguration
+        return super.isEnabledFor(configuration) && configuration is JUnitConfiguration && configuration.repeatMode != repeatMode
     }
 
     override fun updatePresentation(presentation: Presentation, actionText: String, context: ConfigurationContext) {
@@ -33,3 +29,12 @@ class RunUntilStoppedAction() : RunContextAction(DefaultRunExecutor.getRunExecut
         presentation.text = "${presentation.text} and repeat until stopped"
     }
 }
+
+
+class RunUntilStoppedAction : RunDebugUntilStoppedAction(RepeatCount.UNLIMITED, DefaultRunExecutor.getRunExecutorInstance())
+
+class DebugUntilStoppedAction : RunDebugUntilStoppedAction(RepeatCount.UNLIMITED, DefaultDebugExecutor.getDebugExecutorInstance())
+
+class RunOnceAction : RunDebugUntilStoppedAction(RepeatCount.ONCE, DefaultRunExecutor.getRunExecutorInstance())
+
+class DebugOnceAction : RunDebugUntilStoppedAction(RepeatCount.ONCE, DefaultDebugExecutor.getDebugExecutorInstance())
